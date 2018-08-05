109ca1f2a07593dfa1fe494a45af5da66847c041
Demo_S <demosglok@gmail.com>
2018-08-06 02:13:17 +0300

    'initial'

---
+++ src/components/Cell.vue
@@ -0,0 +1,47 @@
+<template>
+  <div :class="['cell', color]">
+	{{resource}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Cell',
+  props: ['terrain'],
+  computed: {
+	color() {
+		switch (this.terrain) {
+			case 1: return 'green';
+			case 2: return 'red';
+			case 3: return 'blue';
+			default: return '';
+		}
+	},
+	resource() {
+		return Math.random() > 0.5 ? 'o' : '*';
+	}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.cell {
+  height: 50px;
+  width: 50px;
+  line-height:50px;
+  margin: auto;
+  padding: auto;
+  color: white;
+  font-weight:bold;
+}
+.red {
+	background-color: red;
+}
+.blue {
+	background-color: blue;
+}
+.green {
+	background-color: green;
+}
+</style>
---
+++ src/components/Field.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+	<table>
+		<tr v-for="(row, rowindex) in field" :key="`row${rowindex}`">
+			<td v-for="(el, colindex) in row" :key="`col${colindex}`">
+			<cell :terrain="el" />
+			</td>
+		</tr>
+	</table>
+  </div>
+</template>
+
+<script>
+import Cell from '@/components/Cell';
+
+
+export default {
+  name: 'Field',
+  data () {
+    return {
+		msg: 'hello',
+      field: [
+	  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+	  [1,1,2,1,1,1,1,3,3,1],
+	  [1,2,2,2,1,1,1,3,1,1],
+	  [1,1,2,1,1,1,1,3,1,1],
+	  [1,1,1,1,1,1,3,1,1,1],
+	  [1,1,1,1,3,3,1,1,1,1],
+	  [1,1,3,3,1,1,2,2,1,1],
+	  [1,1,3,1,1,2,2,2,2,1],
+	  [1,1,3,1,1,2,2,2,2,1],
+	  [1,1,1,1,1,1,2,2,1,1],
+	  [1,1,1,1,1,1,1,1,1,1]
+	  ]
+    }
+  },
+  components: {
+	cell: Cell
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
